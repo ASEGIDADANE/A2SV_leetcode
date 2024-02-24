8d38a61a98152474b84a3eec882fa5f9295bdddf
ASEGIDADANE <142203006+ASEGIDADANE@users.noreply.github.com>
2024-02-24 23:59:38 +0300

    Add solution for Minimum Size Subarray Sum

--- A2SV_leetcode/leetcode/minimum-size-subarray-sum.py
+++ A2SV_leetcode/leetcode/minimum-size-subarray-sum.py
@@ -1,19 +1,18 @@
-class Solution(object):
-    def minSubArrayLen(self, target, nums):
-        """
-        :type target: int
-        :type nums: List[int]
-        :rtype: int
-        """
-        total = 0
-        length = float('inf')
-        l = 0
-        for r in range(len(nums)):
-            total += nums[r]
-            while total >= target and l <= r:
-                length = min(length, r - l + 1)
-                total -= nums[l]
-                l += 1
-        if length == float('inf'):
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l=0
+       
+        total=0
+        min_lenght=float('inf')
+        for r  in range(len(nums)):
+            total+=nums[r]
+            while total>=target:
+                min_lenght=min(min_lenght,r-l+1)
+                total-=nums[l]
+                l+=1
+        if min_lenght==float('inf'):
             return 0
-        return length
+        return min_lenght
+
+
+        
